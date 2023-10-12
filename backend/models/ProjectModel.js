134ad3b26329b5a92620ef85dae2a054d688e1af
Eetu Siponen <17519980+sip1n@users.noreply.github.com>
2023-10-13 00:25:19 +0300

    exampple to projectmodel

--- backend/models/ProjectModel.js
+++ backend/models/ProjectModel.js
@@ -14,3 +14,43 @@ const projectSchema = new Schema({
 const projectModel = model("Project", projectSchema);
 
 export default projectModel;
+
+/* example project document
+request to server // json:
+{
+  "name": "Test project",
+  "description": "This is a test project",
+  "owner": "65285469fc0f35b7f5ad3992",
+  "readWrite":["65285469fc0f35b7f5ad3992","user2_ID"],
+  "readOnly": [],
+  "comments": [
+      {
+          "user": "65285469fc0f35b7f5ad3992",
+          "text": "this is a comment"
+      }
+  ]
+}
+
+response from server // json:
+
+{
+    "name": "Test project",
+    "description": "This is a test project",
+    "owner": "65285469fc0f35b7f5ad3992",
+    "readWrite": [
+        "65285469fc0f35b7f5ad3992",
+        "user2_ID"
+    ],
+    "readOnly": [],
+    "comments": [
+        {
+            "user": "65285469fc0f35b7f5ad3992",
+            "text": "this is a comment"
+        }
+    ],
+    "_id": "652863acbe5ed46c285e84c3",
+    "createdAt": "2023-10-12T21:22:52.239Z",
+    "updatedAt": "2023-10-12T21:22:52.239Z",
+    "__v": 0
+}
+*/
