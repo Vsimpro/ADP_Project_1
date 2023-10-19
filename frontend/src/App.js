a8ee0ee7a478d7ac84660114c38f90c83d1f9087
Saara <saara.ikonen@student.laurea.fi>
2023-10-19 12:00:18 +0300

    Added React components Navbar and Card

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import Card from './components/Card';
 
 function App() {
   return (
-    <>
-      <h1> Hello World! </h1>
-    </>
+    <div className="container p-3">
+    <div>
+      <Navbar />
+      <Card />
+    </div>
+
+      <div className="row">
+        <div className="col-md-6">
+        <Card />
+        </div>
+        <div className="col-md-6">
+        <Card />
+      </div>
+    </div>
+    </div>
   );
 }
-
 export default App;
---
+++ frontend/src/components/Card.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Card() {
+    return(
+
+<div class="card">
+  <div class="card-body">
+    <h2 class="card-title">Title</h2>
+    <p class="card-text">To do list / text</p>
+
+    <div class="centered-elements">
+      <button type="button" class="btn btn-outline-dark">Button</button>
+      
+    </div>
+
+   
+  </div>
+</div>
+
+    );
+}
+export default Card;
