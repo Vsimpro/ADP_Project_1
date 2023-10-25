75512fb298da3ecab0615bdde5ae3969b3c38f19
Eetu Siponen <17519980+sip1n@users.noreply.github.com>
2023-10-26 01:36:03 +0300

    bcrypt, loginroute, remove useless console.logs

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,17 +1,28 @@
 import express from "express";
 import userModel from "../models/UserModel.js";
+import bcrypt from "bcrypt";
 
 const userRouter = express.Router();
 
 /* POST */
-userRouter.post("/create-user", (request, response) => { 
+// this route is used for signing up a new user (could be changed to /signup)
+userRouter.post("/create-user", (request, response) => {
     console.log("[>] POST '/create-user'");
-    console.log("User data", request.body);
 
-    var newUser = new userModel(request.body);
-    newUser.save()
+    // Hash password
+    const { password } = request.body;
+    const saltRounds = 10;
+
+    bcrypt.hash(password, saltRounds)
+        .then((hash) => {
+            console.log("[*] Password hashed!");
+            request.body.password = hash;
+
+            var newUser = new userModel(request.body);
+            return newUser.save();
+        })
         .then((user) => {
-            console.log("[*] User created!", user);
+            console.log("[*] User created!", user._id);
             response.status(201).send(user);
         })
         .catch((error) => {
@@ -20,53 +31,77 @@ userRouter.post("/create-user", (request, response) => {
         });
 });
 
+// Route for logging in a user
+userRouter.post("/login", (request, response) => {
+    console.log("[>] POST '/login'");
+
+    userModel.findOne({ email: request.body.email })
+        .then((user) => {
+            console.log("[*] User found!", user._id);
+
+            bcrypt.compare(request.body.password, user.password)
+                .then((result) => {
+                    console.log("[*] Passwords match!");
+                    response.status(200).send(user);
+                })
+                .catch((error) => {
+                    console.log("[!] Passwords don't match!", error);
+                    response.status(401).send(error);
+                });
+        })
+        .catch((error) => {
+            console.log("[!] User doesn't exist or passwords don't match!", error);
+            response.status(400).send(error);
+        });
+});
+
 /* GET */
 userRouter.get("/get-user/:id", (request, response) => {
-    console.log("[>] GET '/get-user/:id'");
-    console.log("User ID", request.params.id);
+            console.log("[>] GET '/get-user/:id'");
+            console.log("User ID", request.params.id);
 
-    userModel.findById(request.params.id)
-        .then((user) => {
-            console.log("[*] User found!", user);
-            response.status(200).send(user);
-        })
-        .catch((error) => {
-            console.log("[!] Error finding user", error);
-            response.status(404).send(error);
+            userModel.findById(request.params.id)
+                .then((user) => {
+                    console.log("[*] User found!", user._id);
+                    response.status(200).send(user);
+                })
+                .catch((error) => {
+                    console.log("[!] Error finding user", error);
+                    response.status(404).send(error);
+                });
         });
-});
 
-/* PATCH */
-userRouter.patch("/update-user/:id", (request, response) => {
-    console.log("[>] PATCH '/update-user/:id'");
-    console.log("User ID", request.params.id);
-    console.log("User data", request.body);
+    /* PATCH */
+    userRouter.patch("/update-user/:id", (request, response) => {
+        console.log("[>] PATCH '/update-user/:id'");
+        console.log("User ID", request.params.id);
+        console.log("User data", request.body);
 
-    userModel.findByIdAndUpdate(request.params.id, request.body, { new: true })
-        .then((user) => {
-            console.log("[*] User updated!", user);
-            response.status(200).send(user);
-        })
-        .catch((error) => {
-            console.log("[!] Error updating user", error);
-            response.status(400).send(error);
-        });
-});
+        userModel.findByIdAndUpdate(request.params.id, request.body, { new: true })
+            .then((user) => {
+                console.log("[*] User updated!", user);
+                response.status(200).send(user);
+            })
+            .catch((error) => {
+                console.log("[!] Error updating user", error);
+                response.status(400).send(error);
+            });
+    });
 
-/* DELETE */
-userRouter.delete("/delete-user/:id", (request, response) => {
-    console.log("[>] DELETE '/delete-user/:id'");
-    console.log("User ID", request.params.id);
-    
-    userModel.findByIdAndDelete(request.params.id)
-        .then((user) => {
-            console.log("[*] User deleted!", user);
-            response.status(204).send(user);
-        })
-        .catch((error) => {
-            console.log("[!] Error deleting user", error);
-            response.status(400).send(error);
-        });
-});
+    /* DELETE */
+    userRouter.delete("/delete-user/:id", (request, response) => {
+        console.log("[>] DELETE '/delete-user/:id'");
+        console.log("User ID", request.params.id);
 
-export default userRouter;
+        userModel.findByIdAndDelete(request.params.id)
+            .then((user) => {
+                console.log("[*] User deleted!", user._id);
+                response.status(204).send(user);
+            })
+            .catch((error) => {
+                console.log("[!] Error deleting user", error);
+                response.status(400).send(error);
+            });
+    });
+
+    export default userRouter;
