be8d6ba21a9ee59851ab646e91bee595543cb23d
Eetu Siponen <17519980+sip1n@users.noreply.github.com>
2023-10-26 13:33:56 +0300

    formatting

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -57,51 +57,51 @@ userRouter.post("/login", (request, response) => {
 
 /* GET */
 userRouter.get("/get-user/:id", (request, response) => {
-            console.log("[>] GET '/get-user/:id'");
-            console.log("User ID", request.params.id);
+    console.log("[>] GET '/get-user/:id'");
+    console.log("User ID", request.params.id);
 
-            userModel.findById(request.params.id)
-                .then((user) => {
-                    console.log("[*] User found!", user._id);
-                    response.status(200).send(user);
-                })
-                .catch((error) => {
-                    console.log("[!] Error finding user", error);
-                    response.status(404).send(error);
-                });
+    userModel.findById(request.params.id)
+        .then((user) => {
+            console.log("[*] User found!", user._id);
+            response.status(200).send(user);
+        })
+        .catch((error) => {
+            console.log("[!] Error finding user", error);
+            response.status(404).send(error);
         });
+});
 
-    /* PATCH */
-    userRouter.patch("/update-user/:id", (request, response) => {
-        console.log("[>] PATCH '/update-user/:id'");
-        console.log("User ID", request.params.id);
-        console.log("User data", request.body);
+/* PATCH */
+userRouter.patch("/update-user/:id", (request, response) => {
+    console.log("[>] PATCH '/update-user/:id'");
+    console.log("User ID", request.params.id);
+    console.log("User data", request.body);
 
-        userModel.findByIdAndUpdate(request.params.id, request.body, { new: true })
-            .then((user) => {
-                console.log("[*] User updated!", user);
-                response.status(200).send(user);
-            })
-            .catch((error) => {
-                console.log("[!] Error updating user", error);
-                response.status(400).send(error);
-            });
-    });
+    userModel.findByIdAndUpdate(request.params.id, request.body, { new: true })
+        .then((user) => {
+            console.log("[*] User updated!", user);
+            response.status(200).send(user);
+        })
+        .catch((error) => {
+            console.log("[!] Error updating user", error);
+            response.status(400).send(error);
+        });
+});
 
-    /* DELETE */
-    userRouter.delete("/delete-user/:id", (request, response) => {
-        console.log("[>] DELETE '/delete-user/:id'");
-        console.log("User ID", request.params.id);
+/* DELETE */
+userRouter.delete("/delete-user/:id", (request, response) => {
+    console.log("[>] DELETE '/delete-user/:id'");
+    console.log("User ID", request.params.id);
 
-        userModel.findByIdAndDelete(request.params.id)
-            .then((user) => {
-                console.log("[*] User deleted!", user._id);
-                response.status(204).send(user);
-            })
-            .catch((error) => {
-                console.log("[!] Error deleting user", error);
-                response.status(400).send(error);
-            });
-    });
+    userModel.findByIdAndDelete(request.params.id)
+        .then((user) => {
+            console.log("[*] User deleted!", user._id);
+            response.status(204).send(user);
+        })
+        .catch((error) => {
+            console.log("[!] Error deleting user", error);
+            response.status(400).send(error);
+        });
+});
 
-    export default userRouter;
+export default userRouter;
