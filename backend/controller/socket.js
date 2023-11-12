8ffe3bcd4e4c7e7ae9f3fb28cea16061f3daa7b5
Eetu Siponen <17519980+sip1n@users.noreply.github.com>
2023-11-13 01:34:52 +0200

    soketti säätöö

--- backend/controller/socket.js
+++ backend/controller/socket.js
@@ -1,21 +1,22 @@
 export const handleSocketConnections = (io) => {
-	io.on("connection", (socket) => {
-		console.log("[*] Socket.io: User connected. ID: " + socket.id);
+  io.on("connection", (socket) => {
+    console.log("[*] Socket.io: User connected. ID: " + socket.id);
 
-		socket.on("project:join", (projectID) => {
-			console.log("[*] Socket.io: User: " + socket.id + " joined project: " + projectID);
-			socket.join(projectID);
-		});
+    socket.on("project:join", (projectID) => {
+      socket.join(projectID);
+      console.log("[*] Socket.io: User: " + socket.id + " joined project: " + projectID);
+    });
 
-		socket.on("disconnect", () => {
-			console.log("[*] Socket.io: User disconnected. ID: " + socket.id);
-		});
-	});
+    socket.on("project:update", (projectID) => {
+      socket.to(projectID).emit("project:updated");
+      console.log("[*] Socket.io: Project: " + projectID + " updated");
+    });
 
-	const emitProjectUpdate = (projectID) => {
-		io.to(projectID).emit("project:update", projectID);
-	};
+    socket.on("disconnect", () => {
+      console.log("[*] Socket.io: User disconnected. ID: " + socket.id);
+    });
+  });
+}
 
-};
 
 
