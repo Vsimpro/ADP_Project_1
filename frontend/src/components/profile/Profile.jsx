047d6a16e592e3deec359979fa2020dc3742201e
Saara <saara.ikonen@student.laurea.fi>
2023-11-20 23:14:32 +0200

    Siivosin tiedostoa Profile.jsx

--- frontend/src/components/profile/Profile.jsx
+++ frontend/src/components/profile/Profile.jsx
@@ -1,69 +1,55 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './profile.css';
 import ColorSetter from './ColorSetter.js';
 
 const Profile = () => {
-  const [profileColor, setProfileColor] = useState('');
-
-  const handleColorChange = (color) => {
-    setProfileColor(color);
+ 
+  const handlePasswordChange = (event) => {
+    event.preventDefault(); 
+    console.log('Change Password button clicked');
   };
 
-  const handleButtonClick = (event) => {
-    event.preventDefault(); // Prevent the default form submission behavior
-    // Add your logic for the "Change" button functionality here
-    // This function will be called when the "Change" button is clicked
-    console.log('Change button clicked');
+  const handleSaveProfile = (event) => {
+    console.log('Save button clicked');
+    // TODO profiilitietojen tallennus tietokantaan
   };
+
 
   return (
     <div className='container profilePage'>
-      <div className="row">
-        <div className="col-md-3 col-12 order-md-2"></div>
-
-        <div>
-          <h1>Profile</h1>
-          
-          <ColorSetter onColorChange={handleColorChange} />
-          <form>
-            <div className="form-group">
-              <div
-                className="container custom-container text-center"
-                style={{ backgroundColor: profileColor }}
-              >
-                <div className="row justify-content-center">
-                  <div className="col-md-3 d-flex align-items-center"></div>
-                  <div className="col-md-9 d-flex align-items-center"></div>
-                </div>
-              </div>
-
-              <label>Name</label>
-              <input type="text" className="form-control" placeholder="Name" />
-
-              <hr className="my-4" />
-
-              <h3>Account management</h3>
-              <label>Email</label>
-              <input type="text" className="form-control" placeholder="Email" />
-
-              <label>Password</label>
-              <input type="text" className="form-control" placeholder='************' />
-              <button className='btn btn-outline-dark' onClick={handleButtonClick}>
-                Change
-              </button>
-            </div>
-          
-
+      <div>
+        <h1>Profile</h1>
+        <ColorSetter />
+  
+        <form>
+          <div className="form-group">
+            <label>Name</label>
+            <input type="text" className="form-control" placeholder="Name" />
+  
+            <hr className="my-4" />
+  
+            <h3>Account Management</h3>
+            <label>Email</label>
+            <input type="text" className="form-control" placeholder="Email" />
+  
+            <label>Password</label>
+            <input type="text" className="form-control" placeholder='************' />
+            <button className='btn btn-outline-dark' onClick={handlePasswordChange}>
+              Change
+            </button>
+          </div>
+  
           <hr className="my-4" />
           <div className='buttons-container'>
-            <button className='btn btn-profile save'>Save</button>
+            <button className='btn btn-profile save' onClick={handleSaveProfile}>
+              Save
+            </button>
             <hr className="my-4" />
           </div>
-          </form>
-        </div>
+        </form>
       </div>
     </div>
   );
-};
+}  
 
 export default Profile;
