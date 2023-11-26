80da31d9e659595746b0f4aefb99d95e8669dae7
Saara <saara.ikonen@student.laurea.fi>
2023-11-26 16:07:23 +0200

    Navbarin ulkoasua muokattu

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -8,46 +8,42 @@ function Navbar() {
 
   return (
     <div>
-    <nav className="navbar navbar-dark bg-dark fixed-top">
-      <div className="container-fluid">
-        <div className="d-flex">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <div className="container-fluid">
           <a className="navbar-brand" href="/">Home</a>
-          <a className="navbar-brand" href="/">Cards</a>
-          <a className="navbar-brand" href="/Create">Create new</a>
-        </div>
-        
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse" 
-          data-bs-target="#navbarNav" 
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <a className="nav-link" href="/">Home</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/Profile">Profile</a>
-            </li>
-            <li className="nav-item">
+
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link" href="/Profile">Profile</a>
+              </li>
+              <li className='nav-item'>
               <a className="nav-link" href="/">Cards</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href='/' onClick={handleLogout}>Log Out</a>
-            </li>
-          </ul>
+              </li>
+              <li className='nav-item'>
+              <a className="nav-link" href="/create">Create New</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href='/login' onClick={handleLogout}>Log Out</a>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
-  </div>
+      </nav>
+    </div>
   );
 }
 
 export default Navbar;
-
